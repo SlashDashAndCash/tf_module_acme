580522760b1940a5f62a32bd7237827a187f9541
Jakob Pfeiffer <pgp-jkp@pfeiffer.ws>
2022-01-01 10:12:04 +0100

    Use system-configured DNS resolvers by default

--- variables.tf
+++ variables.tf
@@ -9,5 +9,6 @@ variable "acme_server" {
   default = "https://acme-staging-v02.api.letsencrypt.org/directory"
 }
 variable "recursive_nameservers" {
-  default = ["8.8.8.8:53", "8.8.4.4:53"]
+  type    = list(string)
+  default = null
 }
