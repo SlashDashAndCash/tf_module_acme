d418b2bf91a29977fa54ff06521369791be492f4
Jakob Pfeiffer <pgp-jkp@pfeiffer.ws>
2022-01-01 20:48:12 +0100

    sensitive output

--- output.tf
+++ output.tf
@@ -1,5 +1,6 @@
 output "private_key_pem" {
-  value = acme_certificate.certificate.private_key_pem
+  value     = acme_certificate.certificate.private_key_pem
+  sensitive = true
 }
 
 output "certificate_pem" {
@@ -15,9 +16,11 @@ output "fullchain_pem" {
 }
 
 output "certificate_p12" {
-  value = acme_certificate.certificate.certificate_p12
+  value     = acme_certificate.certificate.certificate_p12
+  sensitive = true
 }
 
 output "certificate_p12_password" {
-  value = acme_certificate.certificate.certificate_p12_password
+  value     = acme_certificate.certificate.certificate_p12_password
+  sensitive = true
 }
